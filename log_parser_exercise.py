df27b0cd08d8a95af58e1a037ce96706ed7d8a10
Bashobi Roy <bashobi@gmail.com>
2021-07-02 00:06:42 -0700

    Status in dict

--- log_parser_exercise.py
+++ log_parser_exercise.py
@@ -18,9 +18,12 @@ OPERATION_FAILED: str = 'The operation has failed.'
 # TODO: If none of the 3 above are found, log error "Couldn't find operation log message"
 
 api_status: Dict = {
-    "API_LINE": 1,
+    "API_LINE_NUM": 1,
     "API_LINE": "",
-    "EXEC_STATUS": ""
+    "EXEC_1": "",
+    "EXEC_2": "",
+    "EXEC_3": "",
+    "EXEC_4": ""
 }
 
 curr_api_index: int = 1
@@ -31,26 +34,31 @@ for index, line in enumerate(lines):
     is_operation_fail: bool = OPERATION_FAILED in line
     is_operation_unavail: bool = DUMPFILE_NOT_AVAILABLE in line
     if is_execution_start:
-        print(line, f'    {index}')
+        # print(line, f'    {index}')
         curr_api_index = index
         curr_api_name = line
         # api_status['API_NAME'] = line
     else:
         curr_api_index = 0
     if is_operation_success:
-        print(f'      {DUMPFILE_NOT_AVAILABLE}')
+        api_status = dict(API_LINE_NUM=curr_api_index,
+                          API_NAME=curr_api_name,
+                          EXEC_1=OPERATION_SUCCESS)
     if is_operation_fail:
-        print(f'      {OPERATION_FAILED}')
+        api_status = dict(API_LINE_NUM=curr_api_index,
+                          API_NAME=curr_api_name,
+                          EXEC_3=OPERATION_FAILED)
+        # print(f'      {OPERATION_SUCCESS}')
+        # print(f'      {OPERATION_FAILED}')
     if is_operation_unavail:
-        print(f'      {OPERATION_SUCCESS}')
-    if not is_operation_fail and not is_operation_success and \
-            not is_operation_unavail:
-        print('Anaplan Unreachable')
+        # print(f'      {DUMPFILE_NOT_AVAILABLE}')
+        api_status = dict(API_LINE_NUM=curr_api_index,
+                          API_NAME=curr_api_name,
+                          EXEC_3=DUMPFILE_NOT_AVAILABLE)
+    #     print('Anaplan Unreachable')
 
     if  is_operation_fail or is_operation_success or  is_operation_unavail:
-            api_status = dict(API_LINE=curr_api_index, API_NAME=curr_api_name,
-                              EXEC_STATUS=OPERATION_FAILED or OPERATION_SUCCESS
-                                          or DUMPFILE_NOT_AVAILABLE)
+        pass
 
     print(api_status)
 
