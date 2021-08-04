1be06a9ad8373db5ad7405105aac47e002ad9a44
Bashobi Roy <bashobi@gmail.com>
2021-08-03 23:17:37 -0700

    Handling several types of pattern

--- log_parser_class.py
+++ log_parser_class.py
@@ -1,6 +1,5 @@
 import re
-from typing import Dict
-from typing import List
+from typing import Dict, List, Union
 
 path_to_file: str = 'log_files/anp_user.txt'
 # path_to_file: str = 'log_files/anp_user.txt'
@@ -19,12 +18,12 @@ class Operation:
 
 
 class Colors:
-    WHITE = '\033[0m'  # white (normal)
-    RED = '\033[31m'  # red
-    GREEN = '\033[32m'  # green
-    ORANGE = '\033[33m'  # orange
-    BLUE = '\033[34m'  # blue
-    PURPLE = '\033[35m'  # purple
+    WHITE = '\033[0m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    ORANGE = '\033[33m'
+    BLUE = '\033[34m'
+    PURPLE = '\033[35m'
 
 
 class ANP:
@@ -36,8 +35,10 @@ class ANP:
         '8a81b010721f3bec01722c4420f878e0^5486F999A91246CF9D710D228864764F': 'FY22_NLG_HUB',
         '8a868cd87a25f4cf017a54dbb22c16e3^2FA3B2DE0B544E139FD6BB012D2DA513': 'FY22_NLG',
         '8a868cd87a25f4cf017a54d776c515d7^463B0A00F8FD4FE5839943BCC68D074E': 'FY22_NLG_SNAPSHOT',
-        '8a81b00f6d31dc75016d4f837db81106^8B5B1716E92943B2967B603BAB7A9A8A':
-            'FY22_NLG_RPT',
+        '8a81b00f6d31dc75016d4f837db81106^8B5B1716E92943B2967B603BAB7A9A8A': 'FY22_NLG_RPT',
+        '8a81b011706ef07f0170aa26df7a23b5^CFEA3491AD6B4ED5B5E1CC1703633D4E': 'FY21NLG_UAT',
+        '8a81b011720f725f017212c5c23678d8^914CFB646A074B5886762EDB3BFBC122': 'NLG_HUB',
+        '8a81b011720f725f017212c44cdf788d^EABF24B166F0404DA34A2E00D5E43FE3': 'NLG_HUB_2H',
         '8a81b09c599f3d1a0159ff60f5e12b6d^456E2DFF385942B8BF45B1D465CEBE51': 'MERAKI',
         '8a81b09c599f3d1a0159ff60f5e12b6d^87CA052790A14BCBA9018E8F42ECFC57': 'MERAKI_SFDC_DATA',
         '8a81b0136cc586ba016ce3a8e40f1e1f^7F4DD0136E5A4319B8F64648ACD58D07': 'SCLASS_CUST_EXP',
@@ -46,25 +47,52 @@ class ANP:
         '8a81b00e6ee246a8016f584e8a576ad8^50007A7C50B6462DA430D3D96C2AC584': 'TPX_SCLASS',
         '8a81b0116d31d5a8016d4186055f03ac^0E2C08013002467AADAAEC797AD62275': 'CENT',
         '8a81b00e6d31d425016d3c5e3375097b^FD58CFEA0AA24B8EA96253936B822BB4': 'BKGHUB_ATBE',
-        '8a81b00e6ee246a8016f584e8a576ad8^21AFB617F1574F26BED27B2CC80E5534': 'TPX_FY22'
+        '8a81b00e6ee246a8016f584e8a576ad8^21AFB617F1574F26BED27B2CC80E5534': 'TPX_FY22',
+        '8a81b0106d31d556016d3c59c99941a9^6918C7ACD5A44F24B1D37C8F457EBDFE': 'SAVHUB',
+        '8a81b0106d31d556016d3c59c99941a9^1DF1AF61970242EFAF7E7A2951C4140C': 'HUB'
+
     }
 
 
 class Regex:
     regex_workspace = r'-workspace (.+) -model'
-    regex_model = r'-model (.+) -debug'
+    regex_model_1 = r'-model (.+) -debug'
+    regex_model_2 = r'-model (.+) -action'
+    regex_model_3 = r'-model (.+) -certificate'
+    regex_model_4 = r'-model (.+) -file'
+    regex_model_5 = r'-model (.+) -import'
+    regex_model_6 = r'-model (.+) -export'
+
     regex_process = r'-process (.+)-execute'
+    regex_export = r'-export (.+)-execute'
+    regex_file = r'-file (.+)-put'
+    regex_action = r'-action (.+)-execute'
+    regex_import = r'-import (.+)-execute'
+
+    def xtract_with_regex(self, line: str, regex: str) -> Union[str, None]:
+        rg = re.compile(regex, re.IGNORECASE | re.DOTALL)
+
+        match = rg.search(line)
+        print(f' {line}\n {match}')
+        try:
+            return match.group(1)
+        except AttributeError or IndexError:
+            # breakpoint()
+            return
+
     # print(f'{regex_model}   {regex_process}'
     #       )
 
 
-class Log_Parse:
+class Log_Parse(Regex):
+    # class Log_Parse(Regex):
 
-    def __int__(self,
-                path_to_log_files: str = 'anaplan_logs',
-                path_to_result_files: str = 'log_review'
-                ):
-
+    def __int__(
+            self,
+            path_to_log_files: str = 'anaplan_logs',
+            path_to_result_files: str = 'log_review'
+    ):
+        # self.regex = Regex()
         self.path_to_log_files = path_to_log_files
         self.path_to_return_files = path_to_return_files
 
@@ -72,13 +100,14 @@ class Log_Parse:
     log_parse_dict: Dict = {}
     model_names: Dict = {}
 
-    def constr_model_names_dict(self):
+    def constr_model_names_dict(self, lines: List):
 
         api_status_list: List = []
         curr_api_index: int = 1
         status_api_index: int = 0
         curr_api_name: str = ''
         model_name_counter: int = 0
+        model_name_key: str = 'Unknown Model Name'
         for index, line in enumerate(lines):
 
             is_execution_start: bool = Operation.START in line
@@ -91,25 +120,29 @@ class Log_Parse:
                 model_name_key: str = self.gen_model_name_key(
                     line=line,
                     model_name_counter=model_name_counter)
-                # print(c_log_parse.model_names[model_name_key])
-                # print(f' Print curr_api_name {curr_api_name}')
-                # print(curr_api_name[-3])
-                if curr_api_name[-3] == '*':
-                    curr_api_name = self.model_names[
-                        model_name_key]
-                    curr_api_name += '    Anaplan Unreachable'
-                    # api_status_list.append(curr_api_name)
 
-                    self.model_names[
-                        model_name_key] = curr_api_name
-
+                # breakpoint()
                 curr_api_index = index
                 status_api_index = 0
+                if curr_api_name[-2:] == ' ' or curr_api_name[-2:] == '**':
+
+                    # curr_api_name = self.model_names[model_name_key]
+                    curr_api_name: Union[str, None] = self.model_names.get(
+                        model_name_key)
+                    print(f'Bashobi curr_api_name {curr_api_name}')
+                    if curr_api_name == None:
+                        curr_api_name = 'Model or Workspace not mapped '
+                    curr_api_name += '    Anaplan Unreachable'
+                    print(f'Bashobi curr_api_name {curr_api_name}')
+                    # api_status_list.append(curr_api_name)
+
+                    self.model_names[model_name_key] = curr_api_name
 
             # print(f' Length of key name {model_name_key}')
             if (is_operation_fail or is_operation_unavail or \
                     is_operation_success):
-                if len(model_name_key) > 1:
+                if len(model_name_key) > 1 and (model_name_key != 'Unknown ' \
+                                                                  'Model Name'):
                     status_api_index = 1
                     curr_api_name = self.model_names[
                         model_name_key]
@@ -132,25 +165,49 @@ class Log_Parse:
             if curr_api_index > 0 and status_api_index == 1:
                 api_status_list.append(curr_api_name)
                 curr_api_index = 0
+                print(f'Bashobi...Did it come here   {curr_api_name}')
                 status_api_index = 0
                 self.model_names[
                     model_name_key] = curr_api_name
 
         # print((c_log_parse.model_names))
 
-    def xtract_with_regex(self, line: str, regex: str):
-        rg = re.compile(regex, re.IGNORECASE | re.DOTALL)
-        match = rg.search(line)
-        return match.group(1)
-
     def gen_model_name_key(self, line: str, model_name_counter: int) -> str:
         # print(line)
         # if re.search("./AnaplanClient.sh", line):
-        workspace_token_new = self.xtract_with_regex(line,
-                                                     Regex.regex_workspace)
+        workspace_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_workspace)
 
-        model_token_new = self.xtract_with_regex(line, Regex.regex_model)
-        process_token_new = self.xtract_with_regex(line, Regex.regex_process)
+        model_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_model_1) or self.xtract_with_regex(
+            line,
+            self.regex_model_2) or self.xtract_with_regex(
+            line,
+            self.regex_model_3) or self.xtract_with_regex(
+            line,
+            self.regex_model_4) or self.xtract_with_regex(
+            line,
+            self.regex_model_5) or self.xtract_with_regex(
+            line,
+            self.regex_model_6)
+
+        process_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_process)
+        export_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_export)
+        file_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_file)
+        action_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_action)
+        import_token_new: Union[str, None] = self.xtract_with_regex(
+            line,
+            self.regex_import)
 
         # print(process_token)
         # workspace = self.xtract_with_regex(line, Regex.regex_workspace)
@@ -158,41 +215,36 @@ class Log_Parse:
         # s_workspace = workspace.start()
         # e_workspace = workspace.end()
         #
-        # model = re.search("-model ", line)
-        # debug = re.search("-debug ", line)
-        # process = re.search("-process ", line)
-        # execute = re.search("-execute ", line)
-        # s_model = model.start()
-        # e_model = model.end()
-        # s_debug = debug.start()
-        # s_process = process.start()
-        # e_process = process.end()
-        # s_execute = execute.start()
-        #
         # workspace_token = line[e_workspace:s_model - 1]
-        # model_token = line[e_model:s_debug - 1]
-        # process_token = line[e_process:s_execute - 1]
-        # # print(workspace_token, model_token,process_token)
-        # index_constructed = f'{model_name_counter}:{workspace_token}^{model_token}'
-        # print(f'Old way    {index_constructed}')
         index_constructed_new = f'{model_name_counter}:{workspace_token_new}' \
                                 f'^{model_token_new}'
         # print(f'New way    {index_constructed_new}')
-        self.model_names[index_constructed_new] = process_token_new
-
+        # print(f' Workspace {workspace_token_new}     Model {model_token_new}')
+        print(
+            f'  Is it Process  {process_token_new}  ..or is it '
+            f'Export '
+            f'{export_token_new} .. or is it File ')
+        self.model_names[
+            index_constructed_new] = process_token_new or export_token_new \
+                                     or file_token_new or action_token_new \
+                                     or import_token_new
+        # print(self.model_names)
         return index_constructed_new  # self.model_names
 
     def color_the_dict_output(self, log_parse_dict):
         # print(self.anp_parm)
         for key, value in log_parse_dict.items():
             # print(key, value)
-            is_anaplan_unreachable: bool = Operation.UNREACHABLE in value
-            DUMPFILE_NOT_AVAILABLE: bool = Operation.DUMPFILE_NOT_AVAILABLE in value
-            OPERATION_FAILED: bool = Operation.FAILED in value
-            OPERATION_SUCCESS: bool = Operation.SUCCESS in value
+            if value == None:
+                pass
+            else:
+                is_anaplan_unreachable: bool = Operation.UNREACHABLE in value
+                DUMPFILE_NOT_AVAILABLE: bool = Operation.DUMPFILE_NOT_AVAILABLE in value
+                OPERATION_FAILED: bool = Operation.FAILED in value
+                OPERATION_SUCCESS: bool = Operation.SUCCESS in value
 
-            key_extract: str = key.split(':')[1]
-            # print(f' Second field of key : {key_extract}')
+                key_extract: str = key.split(':')[1]
+                # print(f' Second field of key : {key_extract}')
 
             if key_extract in ANP.parmeters:
                 display_model_operation_status: str = f'{ANP.parmeters[key_extract]}   {value}'
@@ -219,6 +271,43 @@ class Log_Parse:
 
 c_log_parse = Log_Parse(
 )
-c_get_status = c_log_parse.constr_model_names_dict()
-c_get_status_colour = c_log_parse.color_the_dict_output(
+# c_get_status = c_log_parse.constr_model_names_dict(lines)
+# c_get_status_colour = c_log_parse.color_the_dict_output(
+#     c_log_parse.model_names)
+
+path_to_file: str = 'anaplan_logs/xcat_import_export.log'
+# path_to_file: str = 'log_files/anp_user.txt'
+
+with open(file=path_to_file, mode='r') as f:
+    lines = f.readlines()
+f.close()
+c_get_status_1 = c_log_parse.constr_model_names_dict(lines)
+c_get_status_colour_1 = c_log_parse.color_the_dict_output(
     c_log_parse.model_names)
+
+# path_to_file: str = 'anaplan_logs'
+# # path_to_file: str = 'log_files/anp_user.txt'
+#
+# # import os
+#
+# subfiles = []
+# for dirpath, subdirs, files in os.walk(path_to_file):
+#     for x in files:
+#         if x.endswith(".log"):
+#             subfiles.append(os.path.join(dirpath, x))
+# c_log_parse = Log_Parse(
+# )
+#
+# for file in subfiles:
+#     with open(file=file, mode='r') as f:
+#         # while open(file=file, mode='r') as f:
+#         lines = f.readlines()
+#         # print(lines)
+#     f.close()
+#     print(file)
+#     #
+#     c_get_status = c_log_parse.constr_model_names_dict(lines)
+#     # c_get_status_colour = c_log_parse.color_the_dict_output(
+#     #     c_log_parse.model_names)
+#
+# print(subfiles)
